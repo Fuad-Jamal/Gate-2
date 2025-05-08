b908646e9053d7880e9426ca66aa7916a1e5e3fb
Fuad Jamal <jamalfuad34@gmail.com>
2025-05-08 19:22:34 +0200

    div is centered

--- web.js
+++ web.js
@@ -3,3 +3,14 @@ const body = document.body;
 const div = document.createElement('div')
 body.append(div)
 div.innerHTML = 'inject script'
+
+const inhi = document.querySelector('#h1')
+inhi.getAttribute('id')
+
+const didi = document.createElement('div');
+        document.body.appendChild(didi);
+
+        didi.style.margin = 'auto'
+        didi.style.height = '200px'
+        didi.style.width = '200px'
+        didi.style.backgroundColor = 'red'
